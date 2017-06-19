a9656f3dc304afe93d76014ba7f4b6c29f0d551f
maxshalkov <max.shalkov@gmail.com>
2017-06-19 18:32:19 +0300

    Start

---
+++ Resources.ps1
@@ -0,0 +1,67 @@
+﻿function Get-ptIniFile{
+
+    [cmdletbinding()]
+    Param(
+
+        [parameter(Mandatory=$true)]
+        [string]$FilePath
+
+    )
+
+    Begin{
+        $Ini = @{}
+    }
+    
+    Process{
+        Get-Content $FilePath | Where-Object {$_ -match "^[^;]"} | ForEach-Object {
+            switch ($_){
+                {$_ -match "\["} {
+                    $Head = $_ -replace "\[|\]" 
+                    $Ini[$Head] = @()
+                    return
+                }
+
+                {$Head -ne $null} {
+                    $Ini[$Head] += ,$_
+                }
+            }
+        }
+    }
+
+    End{
+        return $Ini
+    }
+}
+
+function New-ptMessageBox{
+
+    [cmdletbinding()]
+    Param(
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Text,
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Head,
+
+        [ValidateSet("Asterisk","Error","Exclamation","Hand","Information","None","Question","Stop","Warning")]
+        [parameter(Mandatory=$false)]
+        [Windows.Forms.MessageBoxIcon]$Type = "Information",
+
+        [ValidateSet("AbortRetryIgnore","OK","OKCancel","RetryCancel","YesNo","YesNoCancel")]
+        [parameter(Mandatory=$false)]
+        [Windows.Forms.MessageBoxButtons]$ButtonType = "OK"
+    
+    )
+
+    Begin{
+        Add-Type -AssemblyName System.Windows.Forms
+    }
+
+    Process{
+        [System.Windows.Forms.MessageBox]::Show($Text, $Head, $ButtonType, $Type)
+    }
+
+}
---
+++ ptADInfoEditor.ps1
@@ -0,0 +1,24 @@
+﻿Clear-Host
+
+#Requires -Version 5.0
+
+Data LocalizedData{ 
+    # culture = "ru-RU" 
+    ConvertFrom-StringData @' 
+        TestError        = __pt: Тестовая ошибка. 
+        FailedImportFunc = __pt: Ошибка при импорте функции {0}"
+'@ 
+} 
+
+Try{
+    . "$PSScriptRoot\New-ptMessageBox.ps1"
+} 
+
+Catch{
+    throw "1"
+}
+<#
+if (-not (Test-Path $PSScriptRoot\Properties.ini)){
+    throw "__pt: File property not found"
+}
+#>
